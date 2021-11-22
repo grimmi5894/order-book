ce0c10f534691bf4d31d5547354fc6d36abfee59
Tricia Brooker <grimmi5894@gmail.com>
2021-11-22 13:43:20 -0500

    created if statement to add if existingBook is empty, started for loop, test 1 passes, all other tests fail

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,17 @@
-const reconcileOrder = () => {
+/* eslint-disable no-console */
+const reconcileOrder = (existingBook, incomingOrder) => {
+  let updatedBook = []
+  // adds an order to the book when the book is empty and thus cannot fulfill the order
 
+  if (existingBook.length === 0) updatedBook.push(incomingOrder)
+  for (let i = 0; i < existingBook.length; i++) {
+    if (incomingOrder !== existingBook[i]) updatedBook.push(incomingOrder)
+  }
+  console.log(updatedBook)
+
+  return updatedBook
 }
+
+
 
 module.exports = reconcileOrder
