50d51368297c8684cc5fa7d160d8af4174a51c32
Tricia Brooker <grimmi5894@gmail.com>
2021-11-22 15:01:47 -0500

    added if statement to for loop to push existingBook[i] to updatedBook at start of iteration, added if statement to push incomingOrder if does not deepequal existingBook[i], 3 tests now pass

--- orderBook.js
+++ orderBook.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-console */
 const reconcileOrder = (existingBook, incomingOrder) => {
   let updatedBook = []
-  // adds an order to the book when the book is empty and thus cannot fulfill the order
 
   if (existingBook.length === 0) updatedBook.push(incomingOrder)
+
+
+  // adds an order to the book when the book has orders of the corresponding type (i.e. a sell with no buys)
+  // adds an order to the book when the book has a corresponding order type but it does not match
+
   for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook.length > 0) updatedBook.push(existingBook[i])
     if (incomingOrder !== existingBook[i]) updatedBook.push(incomingOrder)
   }
   console.log(updatedBook)
@@ -12,6 +17,15 @@ const reconcileOrder = (existingBook, incomingOrder) => {
   return updatedBook
 }
 
+
+// fulfills an order and removes the matching order when the book contains a matching order of the same quantity
+// fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
+// partially fulfills an order, removes the matching order and adds the remainder of the order to the book when the book contains a matching order of a smaller quantity
+// uses two existing orders to completely fulfill an order, removing the matching orders from the book
+// uses two existing orders to completely fulfill an order, removing the first matching order from the book and reducing the second
+// uses two existing orders to partially fulfill an order, removing the matching orders from the book and reducing the incoming order before adding it to the book
+// Extra Credit: it fulfills a mismatched order when both parties benefit
+// Extra Credit: it does not fulfill a mismatched order when it does not benefit both parties
 
 
 module.exports = reconcileOrder
