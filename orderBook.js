9810ad87621f3b14a74b12f9ac8af518ed5d6116
Tricia Brooker <grimmi5894@gmail.com>
2022-02-13 15:15:35 -0500

    found issue causing last 3 tests to fail, ran tests again, all 9 tests now pass

--- orderBook.js
+++ orderBook.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+
 const reconcileOrder = (existingBook, incomingOrder) => {
   if (existingBook.length === 0) {
     existingBook.push(incomingOrder)
@@ -27,17 +28,18 @@ const reconcileOrder = (existingBook, incomingOrder) => {
       (incomingOrder.price === existingBook[i].price) &&
       (incomingOrder.quantity > existingBook[i].quantity)) {
       incomingOrder.quantity -= existingBook[i].quantity
-      existingBook.push(incomingOrder)
+      // existingBook.push(incomingOrder)
       existingBook.splice(i, 1), i--
 
-      return existingBook
+      // return existingBook
     }
   }
-  // adds an order to the book when the book has orders of the corresponding type (i.e. a sell with no buys)
-  // adds an order to the book when the book has a corresponding order type but it does not match
-  // fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
-  // partially fulfills an order, removes the matching order and adds the remainder of the order to the book when 
-  //    the book contains a matching order of a smaller quantity
+  if ((incomingOrder.type === existingBook.type ||
+    incomingOrder.type !== existingBook.type)) {
+    existingBook.push(incomingOrder)
+
+    return existingBook
+  }
   // uses two existing orders to completely fulfill an order, removing the matching orders from the book
   // uses two existing orders to completely fulfill an order, removing the first matching order from the 
   //    book and reducing the second
